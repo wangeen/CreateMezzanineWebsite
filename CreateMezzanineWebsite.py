2f2ec7d0d248153a4dcf90f31a737f6b1fcd9658
jwang <mac@promote.cache-dns.local>
2014-04-18 22:08:03 +0800

    fix some bugs, still not stable

--- CreateMezzanineWebsite.py
+++ CreateMezzanineWebsite.py
@@ -11,10 +11,10 @@ def script(cmd):
 
 def installPackage():
     print "Install Packages"
-    script("pip install south")
-    script("pip install django-compressor")
+    script("sudo pip install south")
+    script("sudo pip install django-compressor")
     print "The following step is a bit slow,  please pay attention."
-    script("pip install mezzanine")
+    script("sudo pip install mezzanine")
     script("source ~/.bashrc")
     pass
 
@@ -36,7 +36,11 @@ def newMezzanineProject():
     os.chdir("..")
     print "Hah, almost done, now please customize your UI."
     print '''
-    From that point on,  a theme in Mezzanine can be implemented entirely as a standard Django app. Simply create a Django app with templates and static directories,  copy the relevant HTML,  CSS and JavaScript files into it from Mezzanine that you wish to modify,  and then add the theme app’s name to your project’s INSTALLED_APPS setting. Be sure to add the theme to the top of the INSTALLED_APPS list,  so that its templates are found before Mezzanine’s versions of the templates.
+    From that point on,
+    a theme in Mezzanine can be implemented entirely as a standard Django app.
+    Simply create a Django app with templates and static directories,
+    copy the relevant HTML,
+    CSS and JavaScript files into it from Mezzanine that you wish to modify,
     '''
     print "Please refer to http://www.rosslaird.com/blog/customizing-mezzanine/ for detail"
     script("python manage.py runserver")
@@ -47,7 +51,7 @@ if __name__  ==  "__main__":
         Script for build a website from Zero
     '''
     # uncomment following line to enable install package
-    installPackage()
+    #installPackage()
     newMezzanineProject()
 
     pass
